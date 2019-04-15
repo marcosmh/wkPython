8e3248914bc794bcdee922f835dd3180aeaa9e72
Marcos Magagaña Hernández <marcosmh@MacBook-Pro-de-Marcos.local>
2019-04-15 09:23:30 -0500

    probando django

--- pd110/src/boletin/views.py
+++ pd110/src/boletin/views.py
@@ -5,13 +5,18 @@ from .models import Registrado
 
 # Create your views here.
 def inicio(request):
+    titulo = "Django"
+    if request.user.is_authenticated:
+        titulo = "Bienvenido %s " %(request.user)
     form = RegForm(request.POST or None)
     if form.is_valid():
         form_data = form.cleaned_data
         correo = form_data.get("email")
         nomb = form_data.get("nombre")
         obj = Registrado.objects.create(email=correo,nombre=nomb)
+
     context = {
+        "titulo" : titulo,
         "el_form" : form,
     }
     return render(request,"inicio.html",context)
